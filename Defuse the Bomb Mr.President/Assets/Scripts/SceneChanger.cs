14dd0902776c4f9958b625e524a5b9d2ad128fe1
BrandonDennis <22bcdennis@macon.k12.mo.us>
2020-11-05 13:45:01 -0600

    Added on Commit's

---
+++ Defuse the Bomb Mr.President/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene (name);
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        Application.Quit();
+    }
+
+}
